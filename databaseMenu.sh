36926a6f08528c879694e5c683bc1177c0b4908c
JustJ17 <mohammedgamal2001@hotmail.com>
2025-10-01 22:15:08 +0300

    the database menu will now appear after finishing commands, checking that a table exists in create table

--- databaseMenu.sh
+++ databaseMenu.sh
@@ -1,41 +1,46 @@
 #!/bin/bash
 
-echo "$1";
-cd "databases/$1";
+echo "$1"
+cd "databases/$1" || return
 
 echo -e "You are now connected to the database called $1 \n"
 PS3="$1 : "
-select ch in "Create a new Table" "List all available tables" "Drop a table" "Insert into a table" "Select from a table" "Delete from a table" "Update a table" "Exit"; do
+while true; do
+    select ch in "Create a new Table" "List all available tables" "Drop a table" "Insert into a table" "Select from a table" "Delete from a table" "Update a table" "Exit"; do
         case "$REPLY" in
         1)
             echo -e "Creating a new Table: \n"
             read -p "Please enter the table name: " tableName
-            touch "$tableName".csv
-            touch "$tableName".meta
-            echo -e "A new table has been created with the name $tableName \n"
-            echo -e "Please define your table structure: \n"
-            first_coloumn_flag=1
-            while true; do
-                read -p "Please enter the column name or type 'done' to finish: " columnName
-                if [ "$columnName" == "done" ]; then
-                    break
-                fi
-                read -p "Please enter the data type for $columnName (e.g., integer, string): " dataType
-                echo "$columnName:$dataType" >> "$tableName".meta
+            if [ -f "$tableName".csv ]; then
+                echo -e "A table with the name $tableName already exits! \n"
+            else
+                touch "$tableName".csv
+                touch "$tableName".meta
+                echo -e "A new table has been created with the name $tableName \n"
+                echo -e "Please define your table structure: \n"
+                first_coloumn_flag=1
+                while true; do
+                    read -p "Please enter the column name or type 'done' to finish: " columnName
+                    if [ "$columnName" == "done" ]; then
+                        break
+                    fi
+                    read -p "Please enter the data type for $columnName (e.g., integer, string): " dataType
+                    echo "$columnName:$dataType" >>"$tableName".meta
 
-                if [ $first_coloumn_flag -eq 1 ]; then
-                    echo -n "$columnName" >> "$tableName".csv
-                    first_coloumn_flag=0
-                else
-                    echo -n ",$columnName" >> "$tableName".csv
-                fi
+                    if [ $first_coloumn_flag -eq 1 ]; then
+                        echo -n "$columnName" >>"$tableName".csv
+                        first_coloumn_flag=0
+                    else
+                        echo -n ",$columnName" >>"$tableName".csv
+                    fi
 
-                echo -e "Column $columnName with data type $dataType added to table $tableName \n"
-            done   
+                    echo -e "Column $columnName with data type $dataType added to table $tableName \n"
+                done
+            fi
             ;;
         2)
             echo -e "Listing all available tables: \n"
-            ls *.csv 2> /dev/null | sed 's/\.csv$//'
+            ls *.csv 2>/dev/null | sed 's/\.csv$//'
             ;;
         3)
             echo -e "Dropping a table \n"
@@ -47,26 +52,29 @@ select ch in "Create a new Table" "List all available tables" "Drop a table" "In
             else
                 echo -e "Table $dropTable does not exist. \n"
             fi
+            echo ""
             ;;
         4)
             echo -e "Inserting into a table \n"
             ;;
-        5)  
+        5)
             echo -e "Selecting from a table \n"
             ;;
-        6)  
+        6)
             echo -e "Deleting from a table \n"
             ;;
-        7)  
+        7)
             echo -e "Updating a table \n"
             ;;
-        8)  
+        8)
             echo -e "Returning to main menu \n"
-            cd ../..;
-            exit;
+            cd ../..
+            exit
             ;;
         *)
             echo -e "please select one of choices, $REPLY is out of range.\n" ## makes sure that the choices is not out of range
             ;;
         esac
+        break;
     done
+done
